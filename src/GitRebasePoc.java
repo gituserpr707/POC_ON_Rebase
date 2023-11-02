b215e043069e5ee8ce3018bfa898eb3c99dc5b27
gituserpr707 <prashantangadi1999@gmail.com>
2023-11-02 17:53:37 +0530

    bug fix

--- src/GitRebasePoc.java
+++ src/GitRebasePoc.java
@@ -5,6 +5,9 @@ public class GitRebasePoc {
     public void display(){
         System.out.println("initial commit");
     }
+    public void gitBugFix(){
+
+    }
         public static void main(String args[]){
 
         }
