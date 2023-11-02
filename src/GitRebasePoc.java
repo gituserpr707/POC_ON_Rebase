c7dd1d627dc6fdd92fd83697d4e49af447e8ad27
gituserpr707 <prashantangadi1999@gmail.com>
2023-11-02 17:50:18 +0530

    feature 2

--- src/GitRebasePoc.java
+++ src/GitRebasePoc.java
@@ -5,7 +5,14 @@ public class GitRebasePoc {
     public void display(){
         System.out.println("initial commit");
     }
+
+    public  void feature2(){
+        System.out.println("branch");
+    }
+
         public static void main(String args[]){
+
+
 
         }
     }
